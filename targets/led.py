74a350b3362cc985aaa85a115511a2552fbebeca
Alex <masha.kondratieva@gmail.com>
2019-10-04 11:28:14 +0300

    Create LED target

---
+++ targets/led.py
@@ -0,0 +1,27 @@
+import cv2
+
+from targets.target_base import TargetBase
+
+
+class Target(TargetBase):
+
+    def __init__(self, main):
+        super().__init__(main)
+        self.exposure = -5
+
+    @staticmethod
+    def filter_contours(contours, hierarchy):
+        correct_contours = []
+
+        if contours is not None:
+            for cnt in contours:
+                if 1000 < cv2.contourArea(cnt) < 10000:
+                    correct_contours.append(cnt)
+
+        return correct_contours
+
+    @staticmethod
+    def draw_contours(filtered_contours, original):
+        if not filtered_contours:
+            return
+        cv2.drawContours(original, filtered_contours, -1, (255, 255, 0), 3)
