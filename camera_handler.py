417073c06bf249e473e2900d8d4ec350d34b41bb
Alex <masha.kondratieva@gmail.com>
2019-10-20 12:45:44 +0300

    Document new camera handler

--- camera_handler.py
+++ camera_handler.py
@@ -6,8 +6,28 @@ from typing import Tuple
 
 
 class CameraHandler(Thread):
+    """
+    An "upper" class for handling cameras. Includes all methods called from cameras in other classes as abstracts.
+    Defaults to the simplest method, sometimes OpenCV and sometimes PICamera. Details on which is which is found in the
+    methods themselves.
 
+    Attributes
+    ----------
+    camera : cv_camera_new.CVCamera or pi_camera_new.PICamera or realsense_new.RealSense or None
+        - initially assigned as None, and later created in its own way in each individual camera handler
+    exit : bool
+        - a flag indicating the run's shut down
+    frame
+        - the current frame read from self.camera
+    """
     def __init__(self, exposure=0, contrast=7):
+        """
+        Create the camera and set exposure and contrast using handler's own methods. Additional parameters may be set in
+        implementations. If the camera does not run on a thread, this entire function can be overwritten, except the
+        creation and assignment of the exit flag.
+        :param exposure: The exposure to set the camera to. Default is 0.
+        :param contrast: The contrast to set the camera to. Default is 7.
+        """
         self.camera = None
         self.create_camera()
         self.set_exposure(exposure)
@@ -22,52 +42,89 @@ class CameraHandler(Thread):
     def start(self) -> None:
         """
         Implementation of Thread.start(). May be overwritten into an empty function if the camera does not use
-        threading, otherwise should just be called as super().
+        threading, otherwise should not be implemented.
         :return: None
         """
         super().start()
 
     @abstractmethod
     def create_camera(self):
+        """
+        Create the camera to be used for the handler. Must be over-written in any implementation.
+        """
         self.camera = None
 
     @abstractmethod
     def set_exposure(self,  exposure: int):
+        """
+        PICamera default.
+        :param exposure: The exposure to set the camera to.
+        """
         self.camera.exposure = exposure
 
     @abstractmethod
     def set_contrast(self, contrast: int):
+        """
+        PICamera default.
+        :param contrast: The contrast to set the camera to.
+        """
         self.camera.contrast = contrast
 
     @abstractmethod
     def set_resolution(self, resolution: Tuple[int, int]):
+        """
+        PICamera default.
+        :param resolution: The resolution to set the camera to, length x width.
+        """
         self.camera.resolution = resolution
 
     @abstractmethod
     def set_fps(self, framerate):
+        """
+        PICamera default.
+        :param framerate: The framerate to set the camera to.
+        """
         self.camera.framerate = framerate
 
     @abstractmethod
     def get_exposure(self):
+        """
+        PICamera default.
+        :return: The camera's actual exposure.
+        """
         return self.camera.exposure
 
     @abstractmethod
     def get_contrast(self):
+        """
+        PICamera default.
+        :return: The camera's actual contrast.
+        """
         return self.camera.contrast
 
     @abstractmethod
     def get_resolution(self):
+        """
+        PICamera default.
+        :return: The camera's actual resolution.
+        """
         return self.camera.resolution
 
     @abstractmethod
     def get_fps(self):
+        """
+        PICamera default.
+        :return: The camera's actual framerate.
+        """
         return self.camera.framerate
 
     @abstractmethod
     def run(self):
         """
-        Implementation of "abstract" Thread run method, stores frames in a class variable. Breaks if exit flag is
-        raised.
+        OpenCV default.
+        Implementation of Thread.run(), stores frames in the class variable by reading from camera. Breaks if exit flag
+        is raised.
+        Must be over-written if camera doesn't use threading, or uses a different algorithm.
         """
         while True:
             if self.exit:
@@ -83,4 +140,8 @@ class CameraHandler(Thread):
 
     @staticmethod
     def log(data):
+        """
+        Display data on console and write to vision.log.
+        :param data: Information.
+        """
         logging.info(data)
