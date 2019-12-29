361478449ed42c724c20c61d215691fda1ae8921
naty <naty.meitav@Gmail.com>
2019-12-29 17:22:57 +0200

    fix reflector_3

--- targets/reflector_3.py
+++ targets/reflector_3.py
@@ -29,11 +29,46 @@ class Target(TargetBase):
                 area = cv2.contourArea(con)
                 convex = cv2.convexHull(con)
                 convex_area = cv2.contourArea(convex)
-                if 1.5 > (convex_area/2)/area > 0.7 or  0.9 < utils.rectangularity(con,"check") < 1.15:
-                    correct_contours.append(con)
+                if 1.5 > (convex_area/2)/area > 0.7 or 0.9 < utils.rectangularity(con,"check") < 1.15:
+                    extLeft = tuple(con[con[:, :, 0].argmin()][0])
+                    extTop = tuple(con[con[:, :, 1].argmin()][0])
+                    extBot = tuple(con[con[:, :, 1].argmax()][0])
+                    distanceTop = (extTop[0]-extLeft[0])/(extTop[1]-extLeft[1])
+                    distanceBot = (extBot[0]-extLeft[0])/(extBot[0]-extLeft[0])
+                    print(correct_contours)
+                    if distanceBot < distanceTop:
+                        correct_contours.append(con)
         return correct_contours
 
     @staticmethod
     def draw_contours(filtered_contours, original):
         if filtered_contours:
             cv2.drawContours(original,filtered_contours, -1, (200,70,100), 3)
+
+    def measurements(self, frame, contours):
+        distances = []
+        distance = None
+        angle = None
+        x = None
+        y = None
+        for cnt in contours:
+            (x, y) = utils.center(cnt)
+            if contours and self.main.results.camera == 'realsense':
+
+                distances.append(self.main.display.camera_provider.get_distance(x, y))
+                distance = min(distances)
+                closest = contours[distances.index(distance)]
+                (x, y) = utils.center(closest)
+                angle = utils.angle(constants.FOCAL_LENGTHS['realsense'], x, frame)
+            elif contours:
+                (x, y) = utils.center(cnt)
+                height = cv2.boundingRect(cnt)[3]
+                F = constants.FOCAL_LENGTHS[self.main.results.camera]
+                distance = utils.distance(F,0.22,height)
+                angle = utils.angle(F, x, frame)
+
+        if distance:
+            cv2.putText(frame, str(distance), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 1,
+                        cv2.LINE_AA)
+
+        return angle, distance, None, None
